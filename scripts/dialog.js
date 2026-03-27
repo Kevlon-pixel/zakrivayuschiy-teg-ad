37b214e2d0f20b6f0ad77b497144ac1042c826d1
Владислав Карпенко <sdtsbwusbkg@gmail.com>
2026-03-27 17:04:00 +0300

    fix: ultra mega super final fix

--- scripts/dialog.js
+++ scripts/dialog.js
@@ -1,5 +1,5 @@
 const saveButton = document.querySelector('#save-button');
-const dialogElement = document.querySelector('#dialog-id');
+const dialogElement = document.querySelector('#dialog-save');
 
 saveButton?.addEventListener('click', () => {
   if (dialogElement && !dialogElement.open) {
