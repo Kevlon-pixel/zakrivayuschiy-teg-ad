fc684f08b6b58ed137b735f992b9e3a0de084c93
Владислав Карпенко <sdtsbwusbkg@gmail.com>
2026-03-27 16:27:28 +0300

    fix: final fix

--- scripts/dialog.js
+++ scripts/dialog.js
@@ -1,12 +1,11 @@
 const saveButton = document.querySelector('#save-button');
 const dialogElement = document.querySelector('#dialog-id');
-const dialogForm = dialogElement?.querySelector('.form');
+const dialogCloseButton = document.querySelector('#dialog-close-button');
 
 saveButton?.addEventListener('click', () => {
   dialogElement?.showModal();
 });
 
-dialogForm?.addEventListener('submit', (event) => {
-  event.preventDefault();
+dialogCloseButton?.addEventListener('click', () => {
   dialogElement?.close();
 });
