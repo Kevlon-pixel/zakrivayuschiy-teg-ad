6fbb423c8503ff48321e788a15a14f288fa6e2e6
Владислав Карпенко <sdtsbwusbkg@gmail.com>
2026-03-27 16:45:18 +0300

    fix: mega super final fix

--- scripts/dialog.js
+++ scripts/dialog.js
@@ -1,8 +1,15 @@
 const saveButton = document.querySelector('#save-button');
 const dialogElement = document.querySelector('#dialog-id');
+const dialogCloseButton = document.querySelector('#dialog-close-button');
 
 saveButton?.addEventListener('click', () => {
   if (dialogElement && !dialogElement.open) {
     dialogElement.showModal();
+  }
+});
+
+dialogCloseButton?.addEventListener('click', () => {
+  if (dialogElement?.open) {
+    dialogElement.close();
   }
 });
